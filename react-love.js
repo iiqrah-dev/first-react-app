52efbae19cdeba3ff9e29a0b4697dc9a604ea367
Iqrah Nadeem <nadeem.iqrah@stu.mmu.ac.uk>
2022-06-26 21:53:11 +0300

    Broke everything into components, sheesh!

--- react-love.js
+++ react-love.js
@@ -1,32 +1,54 @@
-function PageContent(){
+function Header(){
     return(
-
-        <div>
-
-            <header>
-
+        <header>
                 <nav>
                      <img src="react-logo.png" width="40px"></img>
 
+                     <ul className="nav-items">
+                         <li>Pricing</li>
+                         <li>About</li>
+                         <li>Contact</li>
+                     </ul>
                 </nav>
-
-                <h1>Reasons I want to learn React:</h1>
-                
             </header>
+    )
+}
 
-            <ol>
+function Content(){
+    return (
+         <>
+         
+         <h1>Reasons I want to learn React:</h1>
+
+         <ol>
                 <li>Refactor Buildspace Wave Portal code, that thing is a mess right now!</li>
                 <li>I want to make React Dapps with well structured code that I fully understand!</li>
                 <li>Get a job as a web3 developer, or even start my own web3 company! </li>
-            </ol>
+        </ol>
+
+         </>
+    )
+}
 
 
-            <footer>
+function Footer(){
+    return (
+
+
+        <footer>
                 &#169;  2022 iiqrahCreations. All rights reserved.
             </footer>
 
-        </div>
+    )
+}
 
+function PageContent(){
+    return(
+        <div>
+            <Header/>
+            <Content/>
+            <Footer/>
+        </div>
     )
 }
 
